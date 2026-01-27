d880dddc6cefdbef9a97beb2a96db74ccd8824cf
[dyad] <claudiaqueen2006@gmail.com>
2026-01-26 19:06:39 -0500

    [dyad] Añadiendo un efecto visual de estrellas al mover el ratón - wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ProjectsPage from "./pages/ProjectsPage";
+import StarCursorEffect from "@/components/StarCursorEffect"; // Importar el nuevo componente
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <StarCursorEffect /> {/* Renderizar el efecto de estrellas aquí */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
